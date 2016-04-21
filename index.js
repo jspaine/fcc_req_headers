efc9c9dcbd7b278d2cfa5f969996ec03e1e12dda
jspaine <painejs@hotmail.com>
2016-04-21 16:02:26 +0200

    remove unneeded stuff

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function(req, res) {
-  var urlPath = url.parse(req.url).pathname;
-  if (urlPath === '/api/whoami') {
-    res.writeHead(200, {'Content-Type': 'text/json'});
-    res.write(JSON.stringify(parseHeaders(req.headers)));
-  } else {
-    res.writeHead(302, {'Location': '/api/whoami'});
-  }
+  res.writeHead(200, {'Content-Type': 'text/json'});
+  res.write(JSON.stringify(parseHeaders(req.headers)));
   res.end();
-  console.log(req.headers);
 }).listen(process.env.PORT || 8000);
 
 function parseHeaders(headers) {
